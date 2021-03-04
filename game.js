fcdd803696944d2be96abf59cdbbcd692868b8b8
nurgnc <gencbedriyenur@gmail.com>
2021-03-04 14:15:38 +0300

    snake positions were determined and movement was achieved.

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 
 let box = 32;
+let move;
 
 
 // Images
@@ -9,7 +10,7 @@ let ground = new Image();
 ground.src = "images/ground.jpg";
 
 let food = new Image();
-food.src = "images/food.png"
+food.src = "images/food.png";
 
 //Snake Array
 let snake = [];
@@ -19,15 +20,62 @@ snake[0] = {
 }
 
 // Food Object
-let food = {
-    x: Math.floor(Math.random() *17 + 1) * box;
+let foodi = {
+    x: Math.floor(Math.random() *17 + 1) * box,
+    y: Math.floor(Math.random() *15 + 3) * box, 
 }
+
+// Event Listener
+document.addEventListener("keydown", function(event) {
+    if(event.keyCode == 37) {
+        move = "left";
+    }
+    else if(event.keyCode == 38) {
+        move = "top";
+    }
+    else if(event.keyCode == 39) {
+        move = "right";
+    }
+    else if(event.keyCode == 40) {
+        move = "down";
+    }
+    console.log(move)
+})
 
 function draw() {
     for(let i=0; i<snake.length; i++) {
         ctx.fillStyle = (i == 0) ? "black" : "red";
         ctx.fillRect (snake[i].x, snake[i].y, box, box);
     }
+    // snake old position
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(move == "left") {
+        snakeX -= box;
+    }
+    else if(move == "top") {
+        snakeY -= box;
+    }
+    else if(move == "right") {
+        snakeX += box;
+    }
+    else if(move == "down") {
+        snakeY += box;
+    }
+
+    //snake new head
+    let newHead = {
+        x: snakeX, 
+        y: snakeY,
+    }
+    snake.unshift(newHead);
+
+
+
+
+    ctx.drawImage(food, 0, 0, box, box, foodi.x, foodi.y, box, box);
 }
 
 function loop() {
