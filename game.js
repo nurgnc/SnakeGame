2ce5cebfd68f75faceab3d8b0e7e466dfcf8edff
nurgnc <gencbedriyenur@gmail.com>
2021-03-04 17:53:31 +0300

    the snake was allowed to grow as it ate.

--- game.js
+++ game.js
@@ -27,16 +27,17 @@ let foodi = {
 
 // Event Listener
 document.addEventListener("keydown", function(event) {
-    if(event.keyCode == 37) {
+
+    if(event.keyCode == 37 && move!="right") {
         move = "left";
     }
-    else if(event.keyCode == 38) {
+    else if(event.keyCode == 38 && move!="down") {
         move = "top";
     }
-    else if(event.keyCode == 39) {
+    else if(event.keyCode == 39 && move!="left") {
         move = "right";
     }
-    else if(event.keyCode == 40) {
+    else if(event.keyCode == 40 && move!="top") {
         move = "down";
     }
     console.log(move)
@@ -44,24 +45,28 @@ document.addEventListener("keydown", function(event) {
 
 function draw() {
     for(let i=0; i<snake.length; i++) {
-        ctx.fillStyle = (i == 0) ? "black" : "red";
+        ctx.fillStyle = (i == 0) ? "black" : "green";
         ctx.fillRect (snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "#000000";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
     // snake old position
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(move == "left") {
+    
+
+    if(move == "left" && snakeX != box) {
         snakeX -= box;
     }
-    else if(move == "top") {
+    else if(move == "top" && snakeY != 3*box) {
         snakeY -= box;
     }
-    else if(move == "right") {
+    else if(move == "right" && snakeX != 17*box) {
         snakeX += box;
     }
-    else if(move == "down") {
+    else if(move == "down" && snakeY != 17*box) {
         snakeY += box;
     }
 
@@ -69,6 +74,13 @@ function draw() {
     let newHead = {
         x: snakeX, 
         y: snakeY,
+    }
+
+    if(snakeX == foodi.x && snakeY == foodi.y){
+        foodi.x = Math.floor(Math.random() *17 + 1) * box;
+        foodi.y = Math.floor(Math.random() *15 + 3) * box;
+    } else {
+        snake.pop();
     }
     snake.unshift(newHead);
 
